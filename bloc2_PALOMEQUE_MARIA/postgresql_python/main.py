7a51b8bd146cc28096b04c94c237dc7d59a39744
mariapalomeque <146199622+mariapalomeque@users.noreply.github.com>
2025-02-16 15:24:33 +0100

    Actualizacion, insert

--- bloc2_PALOMEQUE_MARIA/postgresql_python/main.py
+++ bloc2_PALOMEQUE_MARIA/postgresql_python/main.py
@@ -1,5 +1,15 @@
 import create_registre as cr
+import read_registre as rr
 
 
 #Trucada per executar la funció a l'arxiu create_registre.py
 cr.create_reg()
+
+results = rr.read_reg()
+for i in results:  
+    print('\n')
+    print('Nom: ' + i[1])
+    print('Adreça: ' + i[2])
+    print('Telèfon: ' + i[3])
+    print('Email: ' + i[4])
+    print('Naixement: ' + i[5])
